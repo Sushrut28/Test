6f95ca8f770e090ed82698d982df1db6e1a591b3
Sushrut <sushrutdiware@gmail.com>
2015-10-24 12:48:30 +0530

    Update GitDemo.java

--- src/gitdemo/GitDemo.java
+++ src/gitdemo/GitDemo.java
@@ -18,7 +18,8 @@ public class GitDemo {
     public static void main(String[] args) {
         // TODO code application logic here
            
-        System.out.println("Hi");
+        System.out.println("Hi
+        ggggg");
     }
     
 }
